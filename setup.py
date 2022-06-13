699ba984e99f8beb32e11c844ed91e37ea4531e2
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2022-06-14 00:58:43 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="barlaman",
+    version="0.0.1",
+    author="ANDAM Amine",
+    author_email="andamamine83@gmail.com",
+    description='Scrape data of Moroccan House of Representatives.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nowabindata/barlaman',
+    keywords=["politics","parliament","webscraping","parliamentary-data","morocco"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
