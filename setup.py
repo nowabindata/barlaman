e7be4e52da5ccc82dbcc452786e03e3a77fc7f4f
AndamAmine <49843367+AmineAndam04@users.noreply.github.com>
2022-11-18 11:02:45 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="barlaman",
-    version="0.0.1",
+    version="0.1.3",
     author="ANDAM Amine",
     author_email="andamamine83@gmail.com",
     description='Scrape data of Moroccan House of Representatives.',
